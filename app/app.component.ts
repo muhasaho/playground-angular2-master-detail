352a3f8b3ecb6cd6585194479feec53153b6d78a
Travis Wiertzema <twiertzema@appareo.com>
2016-02-03 16:21:50 -0600

    added changes from tech challenge

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,23 @@
 import {Component} from 'angular2/core';
-import {PersonListComponent} from "./person.list.component"
+import {RouteConfig, ROUTER_DIRECTIVES, RouterLink} from "angular2/router";
+
+import {PersonListComponent} from "./person.list.component";
+import {PersonDetailComponent} from "./person.detail";
+
 
 @Component({
     selector: 'my-app',
     template: `
     <h1>Master Detail Example</h1>
-    <person-list></person-list>
+    <nav>
+        <a [routerLink]="['PersonList']">Person List</a>
+    </nav>
+    <router-outlet></router-outlet>
     `,
-    directives: [PersonListComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path:'/person-list', name:'PersonList', component:PersonListComponent},
+    {path:'/person-detail/:name/:age', name:'PersonDetail', component:PersonDetailComponent}
+])
 export class AppComponent { }
--- app/person.list.component.ts
+++ app/person.list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "angular2/core";
 import {PersonService} from "./person.service";
 import {Person} from "./person";
+import {Router, RouteParams} from 'angular2/router'
 
 @Component({
     selector: "person-list",
@@ -8,15 +9,6 @@ import {Person} from "./person";
     <ul *ngFor="#person of people">
         <li (click)="onPersonClick(person)" class="person-item">{{person.name}}</li>
     </ul>
-    <div *ngIf="currentPerson">
-        <h3>Current Person</h3>
-        <div>
-            <span>Name: </span><span>{{currentPerson.name}}</span>
-        </div>
-        <div>
-            <span>Age: </span><span>{{currentPerson.age}}</span>
-        </div>
-    </div>
     `,
     providers: [PersonService],
     styles: [`
@@ -28,14 +20,16 @@ import {Person} from "./person";
 export class PersonListComponent implements OnInit{
 
     public people: Person[];
-    public currentPerson: Person;
-    constructor(private _personService: PersonService){}
+    constructor(
+        private _personService: PersonService,
+        private _router: Router
+    ){}
 
     ngOnInit(){
         this._personService.getPeople().then(people => this.people = people);
     }
 
     onPersonClick(person){
-        this.currentPerson = person;
+        this._router.navigate(['PersonDetail', person]);
     }
 }
